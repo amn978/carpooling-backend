c44db4c7b1afaf0087574617964e574ae05e658f
amn978 <128984554+amn978@users.noreply.github.com>
2025-07-19 23:46:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("https://your-backend-url.com/api/login", {
+   const response = await fetch("https://carpooling-backend-production.up.railway.app/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })

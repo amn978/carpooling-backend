9d4fcfcefa4d23b2e3cfbc9deb6bdda72b9b0deb
amn978 <128984554+amn978@users.noreply.github.com>
2025-07-26 19:49:33 +0530

    Update CarpoolingSystemApplication.java

--- src/main/java/com/carpooling/carpoolingsystem/CarpoolingSystemApplication.java
+++ src/main/java/com/carpooling/carpoolingsystem/CarpoolingSystemApplication.java
@@ -2,11 +2,28 @@ package com.carpooling.carpoolingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CarpoolingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(com.carpooling.carpoolingsystem.CarpoolingSystemApplication.class, args);
+		SpringApplication.run(CarpoolingSystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Frontend URLs
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
